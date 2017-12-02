85a95e99eb742ec0a243a454e9378a3e2e5f6eaa
Ankit singh <ankit1243602@gmail.com>
2017-12-02 11:07:44 +0530

    updated stack

--- src/data_structure/stack/StackArray.java
+++ src/data_structure/stack/StackArray.java
@@ -8,10 +8,9 @@ public class StackArray {
 	public StackArray(int size) {
 		this.size = size;
 		arrStack=new Object[this.size];
-		for (int i = 0; i < arrStack.length; i++) {
-
-		}
 		top = -1;
 	}
+	
+	
 
 }

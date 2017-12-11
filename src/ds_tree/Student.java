fc24ff786ebf5de74178bc36103d314ba89c0f83
Ankit singh <ankit1243602@gmail.com>
2017-12-11 21:56:39 +0530

    updated

--- src/ds_tree/Student.java
+++ src/ds_tree/Student.java
@@ -5,12 +5,41 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
-public class Student {
+class TestClass {
+    public static void main(String args[] ) throws Exception {
 
-public static void main(String[] args) {
-	System.out.println("dsasa");
-	
-	
-}
+        Scanner s = new Scanner(System.in);
+        int num=s.nextInt();
+        for(int i=0;i<num;i++){
+            String name = s.next(); 
+            List<Integer> list=new ArrayList<>();
+            String vwls="aeiou";
+            if(! name.contains("_")){
+                System.out.println(1);
+            }
+            else{
+                //int [] arr=new int[];
+                String str="";
+                int len=name.length();
+                int count=0;
+                for(int k=0;k<name.length();k++){
+                    String cc=""+name.charAt(k);
+                    if(name.charAt(k)=='_'){
+                        list.add(count);
+                    }
+                    if(vwls.contains(cc) && !str.contains(cc)){
+                        str+=cc;
+                        count++;
+                    }
+                    
+                }
+                int sum=1;
+                for(int j=0;j<list.size();j++){
+                       sum*=list.get(j); 
+                }
+    System.out.println(sum);
+            }
+        }
+    }
 }
 
